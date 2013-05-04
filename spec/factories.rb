8de5b379cca87e3a691dae766bb04b28eaa7ade0
Fiachna <fiachna.carter@gmail.com>
2013-05-04 20:34:09 +1000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Fiachna Carter"
+    email                 "fcarter@example.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
